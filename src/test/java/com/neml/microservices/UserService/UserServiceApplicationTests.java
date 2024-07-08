32849c97e79025177c8d2188cf42cb139da7e63f
Shantanu Adewar/Development/NeML <neml10636@smbneml.in>
2024-07-08 16:41:42 +0530

    initial commit on user service

---
+++ src/test/java/com/neml/microservices/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neml.microservices.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
